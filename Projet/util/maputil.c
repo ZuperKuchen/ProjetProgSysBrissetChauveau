ae791706b9490f05d9c89fa25827f52a60080e7e
PierreChauveau <piechauveau@emi.u-bordeaux1.fr>
2016-11-22 16:17:55 +0100

    Ajout fonction change size

--- Projet/util/maputil.c
+++ Projet/util/maputil.c
@@ -4,6 +4,57 @@
 #include <fcntl.h>
 #include <string.h>
 
+
+
+void change_size(int newHeight, int newWidth, char *file){
+  int saveMap = open(file, O_RDWR);
+  
+  int width;
+  int height;
+  int nbObj;
+  read(saveMap, &width, sizeof(unsigned));
+  read(saveMap, &height, sizeof(unsigned));
+  read(saveMap, &nbObj, sizeof(unsigned));
+  
+  // On récupere la matrice avant changement de taille
+  int matrice[height][width];
+  for(int y = 0; y < height; y++){
+    for(int x = 0; x < width; x++){
+      read(saveMap, &matrice[y][x], sizeof(int));
+    }
+  }
+
+  // On récupère le reste du fichier
+  int fin = lseek(saveMap, 0, SEEK_END);
+  int debut = lseek(saveMap, sizeof(unsigned)*3 + width*height*sizeof(int), SEEK_SET);
+  char *buffer = malloc(fin - debut);
+  read(saveMap, buffer, fin - debut);
+
+  // On change les info de taille
+  lseek(saveMap, 0, SEEK_SET);
+  write(saveMap, &newWidth, sizeof(unsigned));
+  write(saveMap, &newHeight, sizeof(unsigned));
+  lseek(saveMap, sizeof(unsigned), SEEK_CUR);
+
+  // On écrit la nouvelle matrice
+  int noObject = -1;
+  
+  for(int y = 0; y < newHeight; y++){
+    for(int x = 0; x < newWidth; x++){
+      if(y <= (newHeight - height) || x >= width){
+	write(saveMap, &noObject, sizeof(int));
+      }else{
+	write(saveMap, &matrice[y-(newHeight-height)][x]);
+      }
+    }
+  }
+
+  // On réécrit le reste du fichier
+  write(saveMap, buffer, fin - debut);
+}
+						
+
+
 int main (int argc, char **argv){
 
   //Usage

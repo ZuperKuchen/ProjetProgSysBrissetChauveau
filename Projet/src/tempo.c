750e43f1cbc7b4898423fc60a55668f5781ba6af
Francois Audoy <faudoy@emi.u-bordeaux1.fr>
2016-11-29 16:59:01 +0100

    utilisation de setitimer pour envoyer le signal sigalrm au programme mais le ce n'est pas le bon thread qui le récupere

--- Projet/src/tempo.c
+++ Projet/src/tempo.c
@@ -16,26 +16,43 @@ void handler_sigalrm(int sig){
   printf("je suis le thread %ld\n", pthread_self());
 }
 
+/* Recupere tout les sigalrm */
 void *demon(void *n){
 
-  printf("%ld\n", pthread_self());
+  printf("pid:%d thread:%ld\n",getpid(), pthread_self());
+  
   struct sigaction act;
   act.sa_handler = handler_sigalrm;
   sigemptyset(&act.sa_mask);
   act.sa_flags=0;
-
-  sigset_t mask;
-  sigemptyset(&mask);
-  sigaddset(&mask, SIGALRM);
-  
   sigaction(SIGALRM, &act, NULL);
-  while (1)
+  
+  sigset_t mask;
+  sigfillset(&mask);
+  sigdelset(&mask, SIGALRM);
+   
+  while (1){
     sigsuspend(&mask);
+  }
 
+}
+
+int timer_init(){
+
+  pthread_t pid;
+  pthread_create(&pid, NULL, demon, NULL);
+
+  printf("pid : %d thread parent: %ld\n", getpid(),pthread_self() );
+  
+  pthread_join(pid, NULL);
+ 
+  return 0;
 }
 
 void timer_set(int timer){
   param = timer;
+
+  //timer_init();
 
   struct itimerval time;
   time.it_interval.tv_sec=0;
@@ -43,27 +60,12 @@ void timer_set(int timer){
   time.it_value.tv_sec=0;
   time.it_value.tv_usec=500000;
   setitimer(ITIMER_REAL,&time,NULL);
-  
-}
-
-
-int timer_init(){
-
-  pthread_t pid;
-  pthread_create(&pid, NULL, demon, NULL);
-
-
-  printf("%d\n", getpid());
-  
-  pthread_join(pid, NULL);
- 
-  return 0;
+  timer_init();
 }
 
 int main(){
 
   int set_timer = 2;
-  timer_init();
   timer_set(set_timer);
 
   return 0;

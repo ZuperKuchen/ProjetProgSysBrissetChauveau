f545ab69ce5e5577e16bcb74c6cd9f61e58696fe
Francois Audoy <faudoy@emi.u-bordeaux1.fr>
2016-11-30 14:04:35 +0100

    Màj

--- Projet/src/tempo.c
+++ Projet/src/tempo.c
@@ -1,20 +1,59 @@
 #define _POSIX_SOURCE
 #define _GNU_SOURCE
+#define _XOPEN_SOURCE >= 500
 
-#include <stdio.h>
+#include <SDL.h>
+#include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <sys/time.h>
 #include <signal.h>
 #include <pthread.h>
-#include <signal.h>
-#include <unistd.h>
-#include <sys/time.h>
-//#include "timer.h"
 
-int param;
+#include "timer.h"
+
+// Return number of elapsed µsec since... a long time ago
+static unsigned long get_time (void)
+{
+  struct timeval tv;
+
+  gettimeofday (&tv ,NULL);
+
+  // Only count seconds since beginning of 2016 (not jan 1st, 1970)
+  tv.tv_sec -= 3600UL * 24 * 365 * 46;
+  
+  return tv.tv_sec * 1000000UL + tv.tv_usec;
+}
+
+
+void* param_event;
+/*
+ *Le stockage des différent timer est implémenté sous la forme d'une file de priorité
+ *
+ *param_event stock l'event à faire à la fin du timer
+ *delay stock le temps du timer
+ *next_timer pointe sur le timer suivant si il existe, NULL sinon
+ *same_time pointe sur le timer qui se termine en même temps que lui 
+ */
+typedef struct timer_event{
+  void* param_event;
+  int delay;
+  struct timer_event * next_timer;
+  struct timer_event * same_time;
+}timer_event;
+
+void enfiler_timer(timer_event *premier,timer_event *timer){
+  if(premier == NULL)
+}
 
 void handler_sigalrm(int sig){
-  printf("je suis le thread %ld\n", pthread_self());
+  printf("je suis le thread %ld \n", pthread_self());
 }
+
+
+
+#ifdef PADAWAN
 
 /* Recupere tout les sigalrm */
 void *demon(void *n){
@@ -23,7 +62,7 @@ void *demon(void *n){
   
   struct sigaction act;
   act.sa_handler = handler_sigalrm;
-  sigemptyset(&act.sa_mask);
+  sigfillset(&act.sa_mask);
   act.sa_flags=0;
   sigaction(SIGALRM, &act, NULL);
   
@@ -37,22 +76,29 @@ void *demon(void *n){
 
 }
 
-int timer_init(){
+// timer_init returns 1 if timers are fully implemented, 0 otherwise
+int timer_init (void)
+{
+  printf("pid : %d thread parent: %ld\n", getpid(),pthread_self() );
 
+  //crée le thread qui récupére le sigalrm
   pthread_t pid;
   pthread_create(&pid, NULL, demon, NULL);
+ 
+  //bloque le sigalrm pour le thread principal
+  sigset_t bloquer_sigalrm;
+  sigaddset(&bloquer_sigalrm, SIGALRM);
+  pthread_sigmask(SIG_BLOCK,&bloquer_sigalrm,NULL); 
 
-  printf("pid : %d thread parent: %ld\n", getpid(),pthread_self() );
-  
+  //attend la fin du thread "demon"
   pthread_join(pid, NULL);
  
-  return 0;
+  return 0; // Implementation not ready
 }
 
-void timer_set(int timer){
-  param = timer;
-
-  //timer_init();
+void timer_set (Uint32 delay, void *param)
+{
+param_event = param;
 
   struct itimerval time;
   time.it_interval.tv_sec=0;
@@ -60,13 +106,7 @@ void timer_set(int timer){
   time.it_value.tv_sec=0;
   time.it_value.tv_usec=500000;
   setitimer(ITIMER_REAL,&time,NULL);
-  timer_init();
+
 }
 
-int main(){
-
-  int set_timer = 2;
-  timer_set(set_timer);
-
-  return 0;
-}
+#endif
